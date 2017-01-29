53228134c51d29cda9c390c6088c42ae010c9970
Lauri Jokipii <lauri.jokipii@rapida.fi>
2017-01-29 18:38:35 +0200

    Change used API

--- js/koulutusluokitus.controller.js
+++ js/koulutusluokitus.controller.js
@@ -151,7 +151,7 @@ koodiApp.controller('koodiController', function($scope,$http)
   $scope.koodiOrderReverse = false;
 
   $scope.koodistokoodilkm = 0;
-  $http.get("http://dwitvipuetl.csc.fi/api/json/koulutusluokitus")
+  $http.get("/api/json/koulutusluokitus")
   //$http.get("/koulutusluokitus.json")
   .then(function (response){
     $scope.luokitus = response.data;
