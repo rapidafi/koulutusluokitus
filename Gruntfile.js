44a0525cba788b21e1e2b788b70efc15da8b1940
Lauri Jokipii <lauri.jokipii@rapida.fi>
2018-07-15 17:42:30 +0300

    Add automation with yarn and grunt. Also change coloring scheme.

---
+++ Gruntfile.js
@@ -0,0 +1,145 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  require('load-grunt-tasks')(grunt);
+  require('time-grunt')(grunt);
+
+  grunt.loadNpmTasks('grunt-google-fonts');
+
+  var srcDir = 'src';
+  var distDir = 'dist';
+
+  grunt.initConfig({
+    clean: {
+      files: distDir,
+      options: {
+        force: true
+      }
+    },
+    useminPrepare: {
+      html: [
+        srcDir+'/index.html'
+      ],
+      options: {
+        dest: distDir,
+        flow: {
+          steps: {
+            'css': ['concat','cssmin'],
+            'js': ['concat','uglify']
+          },
+          post: {}
+        }
+      }
+    },
+    usemin: {
+      html: [
+        distDir+'/index.html'
+      ]
+    },
+    copy: {
+      main: {
+        files: [
+          //html:
+          {
+            expand: true,
+            cwd: srcDir,
+            src: [
+              './*.html'
+            ],
+            dest: distDir,
+            options : {
+              noProcess: '**/*.{png,gif,jpg,ico,svg,eot,ttf,woff,woff2}',
+              process: function (content) {
+                return content.replace(/<!-- dev -->.*<!-- enddev -->/g, '')
+                  .replace(/<!-- mustache /g, '')
+                  .replace(/ end mustache -->/g, '');
+              }
+            }
+          },
+          //js:
+          {
+            expand: true,
+            cwd: srcDir,
+            src: [
+              'js/*.js'
+            ],
+            dest: distDir
+          },
+          //css:
+          {
+            expand: true,
+            cwd: srcDir,
+            src: [
+              'css/*.css'
+            ],
+            dest: distDir
+          },
+          //bootstrapfonts:
+          {
+            expand: true,
+            cwd: 'node_modules/bootstrap-css-only',
+            src: [
+              './fonts/**'
+            ],
+            dest: distDir+'/'
+          },
+          //fontawesomefonts:
+          {
+            expand: true,
+            cwd: 'node_modules/font-awesome',
+            src: [
+              './fonts/**'
+            ],
+            dest: distDir+'/'
+          }
+        ]
+      },
+      develop: {
+        expand: true,
+        cwd: srcDir,
+        src: [
+          'koulutusluokitus.json'
+        ],
+        dest: distDir
+      }
+    },
+    googlefonts: {
+      dest: {
+        options: {
+          fontPath: distDir+'/fonts/',
+          httpPath: '../fonts/',
+          cssFile: distDir+'/css/fonts.css',
+          fonts: [
+            {
+              family: 'Open Sans',
+              styles: [
+                400, 700
+              ]
+            }
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', [
+    'clean',
+    'build'
+  ]);
+
+  grunt.registerTask('build', [
+    'useminPrepare',
+    'concat',
+    'uglify',
+    'cssmin',
+    'copy:main',
+    'googlefonts',
+    'usemin'
+  ]);
+
+  grunt.registerTask('dev', [
+    'build',
+    'copy:develop'
+  ]);
+};
