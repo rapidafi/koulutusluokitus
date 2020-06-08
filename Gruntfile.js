0bfe74c28b54ac065f3e980f48f0954d960caeec
Lauri Jokipii <lauri.jokipii@rapida.fi>
2020-06-08 09:01:41 +0300

    Update libraries

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,6 @@ module.exports = function (grunt) {
   require('time-grunt')(grunt);
 
   grunt.loadNpmTasks('grunt-google-fonts');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
   var srcDir = 'src';
   var distDir = 'dist';
@@ -117,16 +116,7 @@ module.exports = function (grunt) {
           ]
         }
       }
-    },
-    watch: {
-      scripts: {
-        files: ['**/*.js','**/*.css','**/*.html'],
-        tasks: ['copy'],
-        options: {
-          spawn: false,
-        },
-      },
-    },
+    }
   });
 
   grunt.registerTask('default', [
